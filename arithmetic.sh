d82ea7f2fe403921c512f4038b6d85c0bf3ca6fd
PrasadPalkar <explorehorizon15@gmail.com>
2020-01-25 11:52:18 +0530

    ADDED:ResultStoredinDictionary

--- arithmetic.sh
+++ arithmetic.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
+declare -A result
 read -p "Enter First No"  a
 read -p "Enter Second No"  b
 read -p "Enter Third No"  c
@@ -6,3 +7,7 @@ w=$(($a+$b*$c))
 x=$(($a*$b+$c))
 y=$(($c+$a/$b))
 z=$(($a%$b+$c))
+result[W]=$w
+result[X]=$x
+result[Y]=$y
+result[Z]=$z
