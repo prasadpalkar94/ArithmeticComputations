9d13724b43657684c0e6e5b042c42c9ed4f1cacd
PrasadPalkar <explorehorizon15@gmail.com>
2020-01-25 11:21:42 +0530

    ADDED:FirstComputation

--- arithmetic.sh
+++ arithmetic.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
-read -p "Enter First No" a
-read -p "Enter Second No" b
-read -p "Enter Third No" c
+read -p "Enter First No"  a
+read -p "Enter Second No"  b
+read -p "Enter Third No"  c
+w=$(($a+$b*$c))
